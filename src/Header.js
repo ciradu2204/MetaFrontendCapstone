b313300d78f32ece848f4b57b123d9dd61aa3f1b
Cynthia Iradukunda <c.iradukund@alustudent.com>
2023-01-25 17:28:00 +0200

    added semantic elements and meta tags

---
+++ src/Header.js
@@ -0,0 +1,10 @@
+
+const Header = () => {
+
+
+    return (
+        <h1></h1>
+    )
+}
+
+export default Header;
---
+++ src/Nav.js
@@ -0,0 +1,16 @@
+
+const Nav = () => {
+
+    return (
+        <nav>
+          <div><img alt="hello" src=""/></div>
+          <div>
+          <ul>
+            <li><a></a></li>
+          </ul>
+          </div>
+        </nav>
+    )
+}
+
+export default Nav
